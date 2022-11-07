b3ae825bb0209420805327aae1e0a6502c91df39
DELL <orielnaim2001@gmail.com>
2022-11-07 16:56:30 +0200

    ex 5

--- SeleniumPackage/login_guru99_test.py
+++ SeleniumPackage/login_guru99_test.py
@@ -3,12 +3,14 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
 
 service_chrome = Service(r"C:\seleniumQA7\chromedriver.exe")
 driver = webdriver.Chrome(service=service_chrome)
 driver.implicitly_wait(5)
 driver.get("https://demo.guru99.com/test/newtours/")
 driver.maximize_window()
+wait = WebDriverWait(driver,5)
 
 # Login
 userName_editbox = driver.find_element(By.NAME, "userName")
@@ -29,22 +31,6 @@ else:
 # Enter to flights page
 driver.find_element(By.LINK_TEXT, "Flights").click()
 sleep(0.5)
-
-# Close the ad
-# try:
-#     driver.find_element(By.CSS_SELECTOR, "div#card>div>div.btn").click()
-#     print("Have ad")
-#     sleep(0.5)
-# except:
-#     print("Not have ad")
-#     pass
-# try:
-#     driver.find_element(By.CSS_SELECTOR, "span.ns-kg9nk-e-16").click()
-#     print("Have ad")
-#     sleep(0.5)
-# except:
-#     print("Not have ad")
-#     pass
 
 # Choose one way and First class
 oneway_button = driver.find_element(By.CSS_SELECTOR, "[value='oneway']")
